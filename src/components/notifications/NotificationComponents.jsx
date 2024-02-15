a842c9d297b4d2fab344c368b6efb7d1267c3544
Eloi Buil Cuadrat <eloixxl2008@gmail.com>
2024-02-15 19:08:47 +0100

    disabling middleware 7

--- src/components/notifications/NotificationComponents.jsx
+++ src/components/notifications/NotificationComponents.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Button, Card, CardBody } from '@nextui-org/react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { motion } from 'framer-motion';
 import { useLocalStorage } from '@/app/hooks/useLocalStorage';
