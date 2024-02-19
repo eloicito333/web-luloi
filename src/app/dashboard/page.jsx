f700c67f38c081ad77a1645cea55e089cd9b3a22
Eloi Buil Cuadrat <eloixxl2008@gmail.com>
2024-02-19 23:19:17 +0100

    minor error from 0.2.0 solved: /dashboard page had no content at all

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -11,7 +11,7 @@ function Dashboard() {
     <div className="p-4 flex flex-col gap-4 items-center h-full bg-gradient-to-t from-indigo-500 via-purple-400 to-pink-300">
       <NotificationComponents notificationState={[hasNotificationPermision, setHasNotificationPermission]} />
       {hasNotificationPermision && (
-        {/* <div>has permission</div> */}
+        <div></div>
       )}
     </div>
   )
